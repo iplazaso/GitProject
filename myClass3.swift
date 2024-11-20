5f6ac5300e486ce574e1e44597f8a6ead0b5e7bd
iplaza <iplaza@prueba.prueba>
2024-11-20 10:39:30 +0100

    Create others properties

--- myClass3.swift
+++ myClass3.swift
@@ -1,4 +1,9 @@
 class myClass3 {
    let myProperty: String
+   let myProperty2: String
+   init(myProperty: String, myProperty2: String) {
+      self.myProperty = myProperty
+      self.myProperty2 = myProperty2
+   }
    
 }
